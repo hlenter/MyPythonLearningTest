f3d5dccfb45757d6ece0b2d27f11143e08438096
Fiery_red <hlenter@163.com>
2018-03-02 14:58:25 +0800

    change to zd423

--- spider/duanzi_spider.py
+++ spider/duanzi_spider.py
@@ -6,15 +6,15 @@ Created on 2018年3月1日
 # -*- coding: utf-8 -*-
 from urllib.request import urlopen, Request
 import re
-class Spider:
-    '''内涵段子爬虫'''
+class Spider(object):
+    '''shopping spider'''
     def loadPage(self,page):
         '''
-        @brief定义一个URL请求网页的方法
-        @param page 需要请求的第几页
-        @return: 返回的页面html
-        '''
-        url = 'http://www.neihan8.com/article/list_5_' + str(page) + '.html'
+                @brief 定义一个URL请求网页的方法
+                @param page 需要请求的第几页
+                @return: 返回的页面html
+                '''
+        url = 'http://www.zdfans.com/page/' + str(page)
         #User-Agent头
         user_agent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0'
         
@@ -22,10 +22,8 @@ class Spider:
         req = Request(url,headers=headers)
         response = urlopen(req)
         html = response.read()
-        html=html.decode('GBK')
-        
-#         找到所有的段子內容<div class="f18 mb20"> </div>
-        pattern = re.compile(r'<div.*?class="f18 mb20">(.*?)</div>')
+        html=html.decode('utf-8')
+        pattern = re.compile(r'target="_blank" title="(.*?)</a>',re.S)
         item_list = pattern.findall(html)
 
         return item_list
@@ -48,10 +46,10 @@ if __name__ == '__main__':
             内涵段子小爬虫
         ======================
     """
-    print ('请按下回车开始')
-    input()
+    print ('-----------')
 
     #定义一个Spider对象
     mySpider = Spider()
-    mySpider.loadPage(1)
+    it_list = mySpider.loadPage(3)
+    mySpider.printOnePage(it_list, 3)
     
